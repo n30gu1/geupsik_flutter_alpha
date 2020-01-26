da38d8fdf106a342eecc7579abf9fd38fe12a1a9
n30gu1 <n30gu1@icloud.com>
2020-01-26 19:23:58 +0900

    designing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:geupsik_flutter_alpha/parser.dart';
-import 'package:intl/intl.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,60 +9,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "SF"
+      ),
       darkTheme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('오늘 급식'),
-        ),
         body: Home()
       ),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    DateFormat formatter = DateFormat('yyyy년 M월 d일 식단표');
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     DateFormat formatter = DateFormat('yyyy년 M월 d일');
 
-    return Padding(
-      padding: EdgeInsets.all(5.0),
-      child: Card(
-        child: FutureBuilder(
-          future: initiate(DateTime.now()),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done)
-            return Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      formatter.format(DateTime.now()),
-                      textScaleFactor: 1.5,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, i) {
-                          return Text(
-                            snapshot.data[i],
-                            textScaleFactor: 1.4,
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              );
-              else
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-          }
-        ),
-      ),
-    );
-  }
-}
+//     return Padding(
+//       padding: EdgeInsets.all(5.0),
+//       child: Card(
+//         child: FutureBuilder(
+//           future: initiate(DateTime.now()),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.done)
+//             return Padding(
+//                 padding: EdgeInsets.all(10.0),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: <Widget>[
+//                     Text(
+//                       formatter.format(DateTime.now()),
+//                       textScaleFactor: 1.5,
+//                       style: TextStyle(fontWeight: FontWeight.bold),
+//                     ),
+//                     Expanded(
+//                       child: ListView.builder(
+//                         itemCount: snapshot.data.length,
+//                         itemBuilder: (context, i) {
+//                           return Text(
+//                             snapshot.data[i],
+//                             textScaleFactor: 1.4,
+//                           );
+//                         },
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               );
+//               else
+//               return Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//           }
+//         ),
+//       ),
+//     );
+//   }
+// }
